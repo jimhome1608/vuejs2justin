0afee7e7b28b32e62c47026fb871953b43e3430e
jimhome1608@gmail.com <jimhome1608@gmail.com>
2017-02-16 10:52:17 +1100

    FIXED ofi_vue.php

--- ofi_vue.php
+++ ofi_vue.php
@@ -1,1 +1,23 @@
-<!DOCTYPE html><html><head><meta charset=utf-8><title>OFI-Weblink</title><meta name=viewport content="width=device-width,height=device-height,initial-scale=1,minimum-scale=1,user-scalable=no"><meta name=apple-mobile-web-app-capable content=yes><link href="https://fonts.googleapis.com/css?family=Andika" rel=stylesheet><link rel=stylesheet href=static/css/reset.css type=text/css><link rel=stylesheet href=static/css/ps.min.css type=text/css><link href=/static/css/app.e388eade8f1051e0fc80fff3ba4688be.css rel=stylesheet></head><body><div id=app data-officeid=1 data-userid=88 data-propertyid=293935></div><script type=text/javascript src=/static/js/manifest.78ff8ff3e1c663e200f8.js></script><script type=text/javascript src=/static/js/vendor.c256dbdfbd5b1f2d023a.js></script><script type=text/javascript src=/static/js/app.c06761cb1f8b07482e52.js></script></body></html>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=utf-8>
+    <title>OFI-Weblink</title>
+    <meta name=viewport content="width=device-width,height=device-height,initial-scale=1,minimum-scale=1,user-scalable=no">
+    <meta name=apple-mobile-web-app-capable content=yes>
+    <link href="https://fonts.googleapis.com/css?family=Andika" rel=stylesheet>
+    <link rel=stylesheet href=/webmultilink/static/css/reset.css type=text/css>
+    <link rel=stylesheet href=/webmultilink/static/css/ps.min.css type=text/css>
+    <link href=/webmultilink/static/css/app.e388eade8f1051e0fc80fff3ba4688be.css rel=stylesheet>
+    <!-- was <link href=/webmultilink/static/css/app.e6ee29e9ce123959e2731376a1feb62b.css rel=stylesheet> -->
+</head>
+<body>
+    <div id=app data-officeid=<?php echo $officeId; ?> data-userid=<?php echo $userId; ?> data-propertyid=<?php echo $propertyId; ?>></div>
+    <script type=text/javascript src=/webmultilink/static/js/manifest.78ff8ff3e1c663e200f8.js></script>
+    <!-- was <script type=text/javascript src=/webmultilink/static/js/manifest.1dd807f9e3d333e74067.js></script> -->
+    <script type=text/javascript src=/webmultilink/static/js/vendor.c256dbdfbd5b1f2d023a.js></script>-->
+    <!-- <script type=text/javascript src=/webmultilink/static/js/vendor.c256dbdfbd5b1f2d023a.js></script> -->
+    <script type=text/javascript src=/webmultilink/static/js/app.c06761cb1f8b07482e52.js></script>
+    <!-- <script type=text/javascript src=/webmultilink/static/js/app.1f00395bf796927079bc.js></script> -->
+</body>
+</html>
